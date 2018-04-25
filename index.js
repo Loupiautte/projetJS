4012a50327ff1cc5d6a16cbc46bb8114d33a60a5
Loupiautte <loupiautte@github.com>
2018-04-25 09:50:31 +0200

    Base du projet

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('Vous êtes à l\'accueil');
+});
+
+
+
+app.use(function(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(404).send('Page introuvable !');
+});
+
+app.listen(8080);
