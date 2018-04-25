c1a7ba6f4582004e8265a0d098f06f59a8cc0f47
Loupiautte <loupiautte@github.com>
2018-04-25 10:23:42 +0200

    Base du projet

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 var express = require('express');
+var bodyParser = require('body-parser'); // Charge le middleware de gestion des paramètres
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 var app = express();
 
-app.get('/', function(req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.send('Vous êtes à l\'accueil');
-});
+//Rendu en screencast
 
+var taches = ['tache_1','tache_2','tache_3']
+
+app.get('/todo', function (req, res) {
+    res.header("Content-Type",'application/json');
+    res.send(JSON.stringify(taches));
+})
+
+app.post('/todo/ajouter', function (req, res) {
+
+})
+
+app.delete('/todo/supprimer/:id', function (req, res) {
+
+})
 
 
 app.use(function(req, res, next){
-    res.setHeader('Content-Type', 'text/plain');
-    res.status(404).send('Page introuvable !');
+
 });
 
 app.listen(8080);
